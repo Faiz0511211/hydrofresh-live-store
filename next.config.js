278120f784c27b79f51df2b3c41523294f947027
Faiz0511211 <mohammadfaiznawab@gmail.com>
2025-07-24 15:47:11 +0530

    Fix next.config.ts to .js filename

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  typescript: {
-    // ignoreBuildErrors: true,
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
